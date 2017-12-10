e23de3cfce3fc4e281e28a70b55df88e93428945
Augie <bal23@students.uwf.edu>
2017-12-09 18:12:36 -0600

    Added environment variables support.

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 import {ImgurApiV3Component} from "../../components/imgur-api-v3/imgur-api-v3";
+import { ENV } from '@app/env'
+
 
 @Component({
   selector: 'page-home',
@@ -13,6 +15,7 @@ export class HomePage {
   constructor(public navCtrl: NavController, public imgur: ImgurApiV3Component) {}
 
   ionViewDidLoad() {
+    console.log(ENV);
   }
 
 
